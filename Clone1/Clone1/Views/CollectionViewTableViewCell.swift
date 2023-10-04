99c94321c06040df77da690602c8b94cee120ddb
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-10-05 00:24:32 +0900

    feat: 테이블뷰 안에 컬렉션뷰 중첩

--- Clone1/Clone1/Views/CollectionViewTableViewCell.swift
+++ Clone1/Clone1/Views/CollectionViewTableViewCell.swift
@@ -10,13 +10,45 @@ import UIKit
 class CollectionViewTableViewCell: UITableViewCell {
 
     static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionview: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collcetionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collcetionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collcetionView
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionview)
+        
+        collectionview.delegate = self
+        collectionview.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionview.frame = contentView.bounds
+    }
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    
 }
